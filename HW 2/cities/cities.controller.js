e47b6b8bdcc8d796a5295ca1291981229af41307
lahanyan <90820417+lahanyan@users.noreply.github.com>
2022-03-29 18:50:59 +0400

    Update cities.controller.js

--- HW 2/cities/cities.controller.js
+++ HW 2/cities/cities.controller.js
@@ -3,8 +3,6 @@ const citiesService = require('./cities.service');
 const asyncHandler = require('express-async-handler');
 const route = express.Router();
 
-
-
 route.get('/:zipCode/', asyncHandler(async (req, res) => {
     const index = req.params['zipCode'];
     const city = await citiesService.getCityByZipCode(index);
